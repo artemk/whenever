1354e2e3dd7a06bb8a284d234b013163e8988e97
Artem Kramarenko <me@artemk.name>
2014-01-06 13:05:41 +0200

    make hook for update crontab configurable

--- lib/whenever/capistrano.rb
+++ lib/whenever/capistrano.rb
@@ -2,7 +2,14 @@ require "whenever/capistrano/recipes"
 
 Capistrano::Configuration.instance(:must_exist).load do
   # Write the new cron jobs near the end.
-  before "deploy:finalize_update", "whenever:update_crontab"
+  # Can be set multiple hooks like {[[:before, "deploy:finalize_update"] , [:after, "deploy:rollback"]]}
+  _cset(:whenever_update_crontab_hook) {[[:before, "deploy:finalize_update"]]}
+
+  fetch(:whenever_update_crontab_hook).each do |_when, _what|
+    #before "deploy:finalize_update", "whenever:update_crontab"
+    self.send(_when.to_sym, _what, "whenever:update_crontab")
+  end
+
   # If anything goes wrong, undo.
   after "deploy:rollback", "whenever:update_crontab"
 end
